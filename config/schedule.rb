76578e7e580d5405618b3f312e4b07b89cb5a60a
epistrephein <epistrephein@gmail.com>
2018-01-08 08:55:58 +0100

    Add cron output for stdout

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,9 @@ job_type :ruby, 'cd :path && bundle exec ruby :task :output'
 env :PATH, ENV['PATH']
 
 set :chronic_options, hours24: true
-set :output, error: File.join(Dir.pwd, 'log', 'errors.log')
+set :output,
+    standard: File.join(Dir.pwd, 'log', 'stdout.log'),
+    error:    File.join(Dir.pwd, 'log', 'stderr.log')
 
 every 1.hour do
   ruby 'hear_ye.rb'
